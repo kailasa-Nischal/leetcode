d1b43148a9ca86a442b38d0a4dffd923919ba32d
kailasa-Nischal <78311217+kailasa-Nischal@users.noreply.github.com>
2022-07-03 11:54:59 +0530

    Create searchInsert.java

---
+++ searchInsert.java
@@ -0,0 +1,12 @@
+int l=0,h=nums.length-1;
+		while(l<=h){
+			int mid=l+(h-l)/2;
+			if(nums[mid]==target)	return mid;
+			else if(nums[mid]>target){
+				h=mid-1;
+			}
+			else{
+				l=mid+1;
+			}
+		}
+		return l;
